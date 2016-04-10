02c9506654bc82553af75ae5ae7215099c3741e7
MBerrendorf <max.berrendorf@rwth-aachen.de>
2016-04-10 14:47:16 +0200

    Camera Projection matrix from fundamental matrix.

--- eigen_versuche/EightPointAlgorithm.h
+++ eigen_versuche/EightPointAlgorithm.h
@@ -110,6 +110,40 @@ Eigen::Matrix<double, 3, 3> EightPointAlgorithm(const Eigen::MatrixXd& matches)
 	return result;
 };
 
+/**
+ * Computes a matrix whose left-multiplication is equivalent to computing the cross product with the given vector.
+ */
+Eigen::Matrix< double , 3 , 3> crossProductMatrix(const Eigen::Matrix< double , 3 , 1> &paramVector) {
+	Eigen::Matrix< double , 3 , 3> crossProductMatrix = Eigen::Matrix<double, 3, 3>().setZero();
+	crossProductMatrix(0,1) = -paramVector(2);
+	crossProductMatrix(0,2) = paramVector(1);
+	crossProductMatrix(1,0) = paramVector(2);
+	crossProductMatrix(1,2) = -paramVector(0);
+	crossProductMatrix(2,0) = -paramVector(1);
+	crossProductMatrix(2,1) = -paramVector(0);
+	return crossProductMatrix;
+}
+
+/**
+ * Reconstructs the camera projection matrix from the fundamental matrix F.
+ */
+Eigen::Matrix<double, 3, 3> reconstructCameraProjectionMatrix (const  Eigen::Matrix<double, 3, 3> &fundamentalMatrix) {
+	// Initialise solver.
+	const Eigen::JacobiSVD<Eigen::Matrix<double, 3, 3>> svd(fundamentalMatrix.transpose(), Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+	// Initiliase right hand side.
+	const Eigen::Matrix<double, 3, 1> rhs = Eigen::Matrix<double, 3, 1>::Zero();
+
+	// Determine epipole
+	const  Eigen::Matrix<double, 3, 1> epipole = svd.solve(rhs);
+
+	// Compute camera projection matrix.
+	const Eigen::Matrix<double, 3, 3> crossProductEpipole = crossProductMatrix(epipole);
+	const Eigen::Matrix<double, 3, 3> cameraProjectionMatrix = - crossProductEpipole *fundamentalMatrix;
+
+	return cameraProjectionMatrix;
+}
+
 } // anon namespace
 
 #endif // EightPointAlgorithm_h_PFJzKM4xWyWc
